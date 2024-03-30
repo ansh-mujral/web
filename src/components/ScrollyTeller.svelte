fc6d526da966ecaba8edb21146b35890bad1f1ce
Ansh Mujral <anshmujral30@gmail.com>
2024-03-30 00:22:38 -0700

    template complete

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -15,62 +15,73 @@
         visualizations_hidden = false;
       }
     }
-
-    // function navigateToSection(sectionIndex) {
-    //     index = sectionIndex;
-    // }
-
-    // let dropdownOpen = false;
-  
-    // function toggleDropdown() {
-    //     dropdownOpen = !dropdownOpen;
-    // }
   
     let sections = [
         {
             title: 'Recipes and Ratings Exploratory Data Analysis Project',
-            content: `This project involves an in-depth exploratory data analysis of a Recipes and Ratings dataset. By applying various statistical methods and visualization techniques, I uncovered insightful trends and patterns that could significantly impact culinary practices and preferences.`,
+            content: `yap`,
+            // content: `This project involves an in-depth exploratory data analysis of a Recipes and Ratings dataset. By applying various statistical methods and visualization techniques, I uncovered insightful trends and patterns that could significantly impact culinary practices and preferences.`,
             imageUrl: 'RReda.png',
             imageClass: 'project-image',
+            projTitle: 'Check out my Recipes and Ratings EDA here!',
             id: 'project1',
-            technologies: 'Technologies used: Python, Pandas, Matplotlib, Seaborn',
+            technologies: 'Technologies used: yap',
             link: 'https://keenans04.github.io/RecipesAndRatings/'
         },
         {
             title: 'Recipes and Ratings Pipelines and Modeling Project',
-            content: `Focusing on automating data processing and modeling for the Recipes and Ratings dataset, this project streamlines the workflow through pipelines. The goal was to enhance efficiency and reproducibility, ensuring that the data analysis process is more structured and accessible.`,
+            // content: `Focusing on automating data processing and modeling for the Recipes and Ratings dataset, this project streamlines the workflow through pipelines. The goal was to enhance efficiency and reproducibility, ensuring that the data analysis process is more structured and accessible.`,
+            content: `yap`,
             imageUrl: 'RRmodel.png', 
             imageClass: 'project-image',
+            projTitle: 'Check out my Recipes and Ratings Model here!',
             id: 'project2',
-            technologies: 'Technologies used: Python, Pandas, Matplotlib, Seaborn',
+            technologies: 'Technologies used: yap',
             link: 'https://keenans04.github.io/WhatsTheRating/'
         },
         {
             title: 'LeGOAT: The LeBron James\'s Career Narrative',
-            content: `A captivating scrollytelling narrative that explores the illustrious career of LeBron James. Using D3.js for interactive storytelling, this project presents LeBron's journey from a promising young talent to one of the greatest basketball players of all time.`,
+            // content: `A captivating scrollytelling narrative that explores the illustrious career of LeBron James. Using D3.js for interactive storytelling, this project presents LeBron's journey from a promising young talent to one of the greatest basketball players of all time.`,
+            content: `yap`,
             imageUrl: 'LeGoat.png',
             imageClass: 'project-image',
+            projTitle: 'Check out my Lebron James Narrative here!',
             id: 'project3',
-            technologies: 'Technologies used: Python, Pandas, Matplotlib, Seaborn',
+            technologies: 'Technologies used: yap',
             link: 'https://keenans04.github.io/LeGOAT/'
         },
         {
             title: 'PowerAtlas: Navigating Global Energy Consumption',
-            content: `PowerAtlas offers an immersive exploration of global energy consumption patterns through interactive data visualizations. This project was designed to provide insights into how different countries consume energy, using advanced data analysis and D3.js to create an engaging, interactive web experience. It highlights the disparities in energy usage and the impact of renewable energy sources across the globe.`,
+            // content: `PowerAtlas offers an immersive exploration of global energy consumption patterns through interactive data visualizations. This project was designed to provide insights into how different countries consume energy, using advanced data analysis and D3.js to create an engaging, interactive web experience. It highlights the disparities in energy usage and the impact of renewable energy sources across the globe.`,
+            content: `yap`,
             imageUrl: 'PowerAtlas.png',
             imageClass: 'project-image',
+            projTitle: 'Check out my PowerAtlas here!',
             id: 'project4',
-            technologies: 'Technologies used: Python, Pandas, Matplotlib, Seaborn',
+            technologies: 'Technologies used: yap',
             link: 'https://keenans04.github.io/PowerAtlas/'
         },
         {
             title: 'Valorant EDA: Analyzing the Skill Gap',
-            content: `This exploratory data analysis (EDA) dives deep into the competitive world of Valorant, analyzing the skill gap between individual players from different regions. By scrutinizing player data and performance metrics, this project sheds light on the factors that contribute to a player's success and how different regions compare in terms of skill level and competitive play style.`,
+            // content: `This exploratory data analysis (EDA) dives deep into the competitive world of Valorant, analyzing the skill gap between individual players from different regions. By scrutinizing player data and performance metrics, this project sheds light on the factors that contribute to a player's success and how different regions compare in terms of skill level and competitive play style.`,
+            content: `yap`,
             imageUrl: 'LeGoat.png',
             imageClass: 'project-image',
+            projTitle: 'Check out my Valorant EDA here!',
             id: 'project5',
-            technologies: 'Technologies used: Python, Pandas, Matplotlib, Seaborn',
+            technologies: 'Technologies used: yap',
             link: 'https://github.com/ansh-mujral/Valorant_EDA' 
+        },
+        {
+            title: 'Contact Me',
+            type: 'contact',
+            content: `Your descriptive text here. Feel free to reach out through any of the following methods:`,
+            contactMethods: [
+                { name: 'Email', icon: 'fas fa-envelope', link: 'mailto:amujral@ucsd.edu' },
+                { name: 'LinkedIn', icon: 'fab fa-linkedin', link: 'https://www.linkedin.com/in/amujral/' },
+                { name: 'GitHub', icon: 'fab fa-github', link: 'https://github.com/ansh-mujral' },
+                { name: 'Resume', icon: 'fas fa-file-text-o', link: 'AnshMujralResume_Mar2024.pdf' },
+            ]
         }
     ];
   
@@ -91,74 +102,58 @@
     </div>
       
     <div class="foreground" slot="foreground">
-        <!-- <div class="dropdown">
-            <button class="dropbtn" on:click={toggleDropdown}>Menu</button>
-            {#if dropdownOpen}
-                <div class="dropdown-content">
-                    {#each sections as section, i}
-                        <a href="javascript:void(0)" on:click={() => {navigateToSection(i); toggleDropdown();}}>{section.title}</a>
-                    {/each}
+        <section id="about-me" class="hero">
+            <h1 class="name" >Ansh Mujral: About Me</h1>
+            <div class="about-me-grid">
+                <div class="about-me-column">
+                    <h2>Interests and Hobbies</h2>
+                    <p>Your interests and hobbies here.</p>
                 </div>
-            {/if}
-        </div> -->
-      <section id="about-me" class="hero">
-        <h1>Ansh Mujral: About Me</h1>
-        <h2 class='subhead'>Hi! Welcome to my website. I am a second-year Data Science major at UC San Diego.<br><br>
-            My journey in Data Science and Machine Learning is fueled by a profound passion, and my goal is to leverage these skills for the betterment of society. If you have any questions, comments, or simply want to engage in a conversation, feel free to drop me an email. I'm always open to connecting and discussing the exciting world of data!</h2>
-      </section>
-        {#each sections as section, i}
-        <section class="scrollable-section">
-            {#if i+1 === index}
-            <div class="content" id={section.id} in:fade={{duration: 400 }} out:fade={{duration: 500 }}>
-                <div class="project-description">
-                    <h1 class='title-content'>{section.title}</h1>
-                    <p>{section.content}</p>
-                    <p>{section.technologies}</p>
-                    <a href={section.link} target="_blank" rel="noopener noreferrer">Check out My Project Here!</a>
-                    <br>
-                    {#if section.imageUrl}
-                        <img src={section.imageUrl} alt={section.title} class="project-image"/>
-                    {/if}
+                <div class="about-me-column">
+                    <h2>About Me</h2>
+                    <p>Your basic description here.</p>
+                    <img class="me" src="me.png" alt="Ansh Mujral">
+                </div>
+                <div class="about-me-column">
+                    <h2>Experiences</h2>
+                    <p>Your experiences here.</p>
                 </div>
             </div>
+        </section>
+        {#each sections as section, i}
+        <section class="scrollable-section">
+            {#if section.type === 'contact'}
+              {#if i+1 === index}
+                    <div class="content" in:fade={{duration: 400}} out:fade={{duration: 500}}>
+                        <h1 class='title-content'>{section.title}</h1>
+                        <p>{section.content}</p>
+                        <div class="contact-methods">
+                            {#each section.contactMethods as method}
+                                <div class="contact-method">
+                                    <a href={method.link} title={method.name}><i class={method.icon}></i> {method.name}</a>
+                                </div>
+                            {/each}
+                        </div>
+                    </div>
+                {/if}
+              {:else}
+                {#if i+1 === index}
+                    <div class="content" in:fade={{duration: 400}} out:fade={{duration: 500}}>
+                        <div class="project-description">
+                            <h1 class='title-content'>{section.title}</h1>
+                            <p>{section.content}</p>
+                            <p>{section.technologies}</p>
+                            <a href={section.link} target="_blank" rel="noopener noreferrer">Check out My Project Here!</a>
+                            <br>
+                            {#if section.imageUrl}
+                                <img src={section.imageUrl} alt={section.title} class="project-image"/>
+                            {/if}
+                        </div>
+                    </div>
+                {/if}
             {/if}
         </section>
         {/each}
-        <!-- <section class="container">
-            <div>
-                <h1>Contact Me</h1>
-            </div>
-            <div class="contact-item">
-                <h2>Email Me</h2>
-                <a href="mailto:amujral@ucsd.edu" class="contact-link">Send an Email</a>
-            </div>
-            <div class="contact-item">
-                <h2>GitHub</h2>
-                <a href="https://github.com/ansh-mujral" class="contact-link">Visit GitHub</a>
-            </div>
-            <div class="contact-item">
-                <h2>Resume</h2>
-                <a href="path/to/your/resume.pdf" class="contact-link" target="_blank">View Resume</a> 
-            </div>
-            <div class="contact-item">
-                <h2>LinkedIn</h2>
-                <a href="https://www.linkedin.com/in/amujral/" class="contact-link">Connect on LinkedIn</a>
-            </div>
-        </section> -->
-        <section id="contact">
-            <div>
-                <h1>Contact Me!</h1>
-                <p>some yap</p>
-            </div>
-            <footer>
-                <ul class="icons">
-                    <li><a href="mailto:amujral@ucsd.edu" title="Email"><i class="fas fa-envelope"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/amujral/" title="LinkedIn"><i class="fab fa-linkedin"></i></a></li>
-                    <li><a href="https://github.com/ansh-mujral" title="Github"><i class="fab fa-github"></i></a></li>
-                    <li><a href="AnshMujralResume_Mar2024.pdf" title="Resume"><i class="fas fa-file-text"></i></a></li>
-                </ul>
-            </footer>
-        </section>
     </div>
   </Scroller>
   
@@ -191,12 +186,67 @@
     }
   
     :global(body) {
-        background: linear-gradient(to right, #0f0c29, #302b63, #24243e);
+        background: linear-gradient(to right, #c89f76, #dbc6a5, #d2b48d); 
     }
-  
+    .about-me-grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 20px;
+        padding: 20px;
+        animation: slideInBottom 0.75s ease-out forwards;
+    }
+
+    .name{
+      animation: slideInFromTop 0.75s ease-out forwards;
+    }
+    /* #about-me{
+      animation: slideInFromLeft 1s ease-out forwards;
+    } */
+
+    .about-me-column {
+        background-color: rgba(255, 255, 255, 0.7);
+        border-radius: 15px;
+        padding: 20px;
+        text-align: center;
+        border: black 2px solid;
+    }
+    .contact-description {
+        background-color: rgba(255, 255, 255, 0.8);
+        margin: 20px 0;
+        padding: 15px;
+        border-radius: 10px;
+    }
+
+    .contact-methods {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    .contact-method {
+        /* margin: 10px;
+        padding: 15px; */
+    }
+
+    .contact-method a {
+        text-decoration: none;
+        color: #007bff; 
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+    .contact-method a:hover {
+        color: #0056b3;
+        text-decoration: underline;
+    }
+    
     .subhead {
       animation: slideInFromTop 0.5s ease-out forwards;
     }
+
     section {
       font-family: 'Monaco', sans-serif;
       height: 100vh;
@@ -229,7 +279,7 @@
     }
 
     .hero{
-        color: white;
+        color: black;
     }
   
     .content {
@@ -254,12 +304,11 @@
     .content img{
         padding: 10px;
     }
+    .me {
+      height: 40vh;
+    }
 
     .container {
-      /* display: grid;
-      grid-template-columns: 3fr 1fr; 
-      grid-template-rows: auto 1fr;
-      gap: 20px; */
       height: 95vh;
     }
     h1 {
@@ -279,52 +328,12 @@
     padding: 20px;
     }
 
-    .dropbtn {
-    background-color: #4CAF50;
-    color: white;
-    padding: 16px;
-    font-size: 16px;
-    border: none;
-    cursor: pointer;
-    }
-
-    /* Dropdown Content */
-    .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-    }
-
-    /* Links inside the dropdown */
-    .dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    }
-
-    /* Change color of dropdown links on hover */
-    .dropdown-content a:hover {background-color: #f1f1f1}
-
-    /* Show the dropdown menu on hover */
-    .dropdown:hover .dropdown-content {
-    display: block;
-    }
-
-    /* Change the background color of the dropdown button when the dropdown content is shown */
-    .dropdown:hover .dropbtn {
-    background-color: #3e8e41;
-    }
-
     .container {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  gap: 10px; /* Reduced gap between the boxes */
-  padding: 40px; /* Padding for the outer container */
+  gap: 10px;
+  padding: 40px;
   justify-items: center;
   align-items: center;
 }
@@ -338,8 +347,8 @@
   padding: 20px;
   background-color: rgba(255, 255, 255, 0.7);
   border-radius: 15px;
-  margin: 5px; /* Ensures boxes are closer together */
-  width: calc(50% - 10px); /* Adjusts width for closer proximity */
+  margin: 5px; 
+  width: calc(50% - 10px);
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
@@ -349,12 +358,6 @@
   font-weight: bold;
   margin-top: 15px;
 }
-
-/* h1 {
-  grid-column: 1 / -1;
-  text-align: center;
-  color: white; 
-} */
   
   </style>
   
